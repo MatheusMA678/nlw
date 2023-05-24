9683f3b5dbef91d7c33263a909d55a8241ffb54e
Matheus Mattos Avelino <114790698+MatheusMA678@users.noreply.github.com>
2023-05-24 17:47:04 +0000

    sistema de Logout adicionado

--- spacetime/server/src/server.ts
+++ spacetime/server/src/server.ts
@@ -21,5 +21,6 @@ app.register(authRoutes)
 app
   .listen({
     port: 3333,
+    // host: '0.0.0.0',
   })
   .then(() => console.log(`Servidor rodando em http://localhost:3333`))
--- spacetime/web/src/lib/api.ts
+++ spacetime/web/src/lib/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    'https://matheusma678-crispy-zebra-rqq5p956vw52pxgp-3333.preview.app.github.dev/',
 })
