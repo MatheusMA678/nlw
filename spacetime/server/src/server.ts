586690cf42b76ae108bd85aa88592b5f19ab84dd
Matheus Mattos Avelino <matheusma2510@hotmail.com>
2023-05-25 00:44:29 -0400

    fix: host problem

--- spacetime/server/src/server.ts
+++ spacetime/server/src/server.ts
@@ -21,6 +21,6 @@ app.register(authRoutes)
 app
   .listen({
     port: 3333,
-    // host: '0.0.0.0',
+    host: '0.0.0.0',
   })
   .then(() => console.log(`Servidor rodando em http://localhost:3333`))
