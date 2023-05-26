8111cc01980e08e8107bf5f1443cfc4dd7389e93
Matheus Mattos Avelino <matheusma2510@hotmail.com>
2023-05-26 14:51:04 -0400

    Aula 4 - Upload de imagens configurado

--- spacetime/server/src/server.ts
+++ spacetime/server/src/server.ts
@@ -1,26 +1,36 @@
-import fastify from 'fastify'
-import cors from '@fastify/cors'
-import jwt from '@fastify/jwt'
-import 'dotenv/config'
+import "dotenv/config";
+import fastify from "fastify";
 
-import { authRoutes } from './routes/auth'
-import { memoriesRoutes } from './routes/memories'
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
 
-const app = fastify()
+import { authRoutes } from "./routes/auth";
+import { memoriesRoutes } from "./routes/memories";
+import { uploadRoutes } from "./routes/upload";
+import { resolve } from "node:path";
 
+const app = fastify();
+
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads",
+});
+app.register(multipart);
 app.register(cors, {
   origin: true,
-})
+});
 app.register(jwt, {
-  secret: 'spacetime',
-})
+  secret: "spacetime",
+});
 
-app.register(memoriesRoutes)
-app.register(authRoutes)
+app.register(authRoutes);
+app.register(uploadRoutes);
+app.register(memoriesRoutes);
 
 app
   .listen({
     port: 3333,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
   })
-  .then(() => console.log(`Servidor rodando em http://localhost:3333`))
+  .then(() => console.log(`🚀 HTTP Server running on http://localhost:3333`));
